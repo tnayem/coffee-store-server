8cd71dd431e605f9acfe553653bc0fc4b264d190
Md Tasdid Nayem <tasdidnayem@gmail.com>
2024-09-08 02:44:47 +0600

    implement

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json())
@@ -28,7 +28,7 @@ async function run() {
         await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Pinged your deployment. You successfully connected to my MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
